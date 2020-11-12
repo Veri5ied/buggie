cca5b5fed772b8dd36763c842d00925f0a12c12a
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-11-12 09:16:04 +0100

    client side navigation

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,12 @@
+import Link from "next/link"
+
 export default function FirstPost() {
     return (
+        <>
         <h1>First Post</h1>
+        <Link href="/">
+            <a>Back to Home</a>
+        </Link>
+        </>
     )
 }
